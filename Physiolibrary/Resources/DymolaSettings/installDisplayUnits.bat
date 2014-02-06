dab608995d02d4c9a9381d293ae299ebb84b96e6
Marek Matejak <marek@matfyz.cz>
2014-02-06 11:28:22 +0100

    better backup for default file Dymola\insert\displayunit.mos

--- Physiolibrary/Resources/DymolaSettings/installDisplayUnits.bat
+++ Physiolibrary/Resources/DymolaSettings/installDisplayUnits.bat
@@ -10,6 +10,7 @@ if "%1"=="" set DYMOLADIR=%DEFAULT_DYMOLADIR%
 
 echo Chosen Dymola directory is "%DYMOLADIR%".
 
+copy "%DYMOLADIR%\insert\displayunit.mos" "displayunit.mos-install_%date%_%time::=-%.bak"
 if not exist "displayunit.mos.bak" copy "%DYMOLADIR%\insert\displayunit.mos" "displayunit.mos.bak"
 xcopy displayunit.mos "%DYMOLADIR%\insert\"
 
