ae0dd2239ccaa6e66f736cea8576f34f1efa1981
Marek Matejak <marek@matfyz.cz>
2015-02-16 18:07:51 +0100

    update of installation scripts

--- Physiolibrary/Resources/Install/Dymola/install.bat
+++ Physiolibrary/Resources/Install/Dymola/install.bat
@@ -1,13 +1,19 @@
 @echo off
 cls
 
+
 set CurrentDirectory=%cd%
 echo %~dp0
 cd %~dp0\..\..\..
 set PHYSIOLIBRARYDIR=%cd%
 
+if not exist "%PHYSIOLIBRARYDIR%\Resources\Install\Dymola" ( 
+   echo "The Physiolibrary directory '%PHYSIOLIBRARYDIR%' is not valid!"
+   exit /B
+) 
+
+
 FOR /f "delims=!" %%i IN ("%PHYSIOLIBRARYDIR%") DO (set PHYSIOLIBRARY=%%~nxi)
-echo %PHYSIOLIBRARY%
 
 
 rem ****** Check administration privileges (for copying files into Dymola Program-Files directory)  *****
@@ -24,7 +30,7 @@ call set DYMOLADIR=%%x:\bin%x:*\bin=%=%%
 set x=
 
 if "%DYMOLADIR%"=="%DYMOLADIR:Dymola=%" ( set ISOK=N ) ELSE ( set ISOK=Y )
-choice /C YN /M "Do you want to install Physiolibrary into dymola directory '%DYMOLADIR%'? " /T 3 /D %ISOK%
+choice /C YN /M "Do you want to install Physiolibrary into dymola directory '%DYMOLADIR%'? " /T 10 /D %ISOK%
 if errorlevel == 2 (
   set DYMOLADIR=
   set /P DYMOLADIR="Please write the Dymola directory:"
@@ -36,11 +42,11 @@ set DYMOLADIR=%DYMOLADIR:"=%
 
 rem ****** Backup and generate "uninstall.bat" *****
 
-if NOT EXIST "%CurrentDirectory%\backup" mkdir "%CurrentDirectory%\backup"
-set DISPLAYUNIT_BACKUP_FILE=%CurrentDirectory%\backup\displayunit.mos-install_%date%_%time::=-%.bak
+if NOT EXIST "%PHYSIOLIBRARYDIR%\Resources\Install\Dymola\backup" mkdir "%PHYSIOLIBRARYDIR%\Resources\Install\Dymola\backup"
+set DISPLAYUNIT_BACKUP_FILE=%PHYSIOLIBRARYDIR%\Resources\Install\Dymola\backup\displayunit.mos-install_%date%_%time::=-%.bak
 copy "%DYMOLADIR%\insert\displayunit.mos" "%DISPLAYUNIT_BACKUP_FILE%"
 
-set UNINST=%CurrentDirectory%\uninstall.bat
+set UNINST=%PHYSIOLIBRARYDIR%\Resources\Install\Dymola\uninstall.bat
 
 echo NET FILE 1^>NUL 2^>NUL > "%UNINST%"
 echo if '%%errorlevel%%' == '0'  goto gotPrivileges >> "%UNINST%"
@@ -49,16 +55,15 @@ echo ECHO UAC.ShellExecute "%PHYSIOLIBRARYDIR%\Resources\Install\Dymola\uninstal
 echo "%%temp%%\OEgetPrivileges.vbs" >> "%UNINST%"
 echo exit /B >> "%UNINST%"
 echo :gotPrivileges >> "%UNINST%"
-echo copy "%DYMOLADIR%\insert\displayunit.mos" "%CurrentDirectory%\backup\displayunit.mos-uninstall_%%date%%_%%time::=-%%.bak" >>  "%UNINST%"
+echo copy "%DYMOLADIR%\insert\displayunit.mos" "%PHYSIOLIBRARYDIR%\Resources\Install\Dymola\backup\displayunit.mos-uninstall_%%date%%_%%time::=-%%.bak" >>  "%UNINST%"
 echo xcopy /Y "%DISPLAYUNIT_BACKUP_FILE%" "%DYMOLADIR%\insert\displayunit.mos" >> "%UNINST%"
 if "%PHYSIOLIBRARY%" NEQ "" echo rmdir /S /Q "%DYMOLADIR%\Modelica\Library\%PHYSIOLIBRARY%" >> "%UNINST%"
-
 
 
 rem ****** Copy files into ProgramFiles\Dymola directory *****
 
 if not exist "%DYMOLADIR%\Modelica\Library" ( 
-   echo "The Dymola directory '%DYMOLADIR%' is not valid?!"
+   echo "The Dymola directory '%DYMOLADIR%' is not valid!"
    exit /B
 ) 
 
